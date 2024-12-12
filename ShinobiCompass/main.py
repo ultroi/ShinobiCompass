848edb4af41c7d61c0e88520e452b57bbd78dabe
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-12-12 11:03:22 +0530

    Update main.py

--- ShinobiCompass/main.py
+++ ShinobiCompass/main.py
@@ -16,3 +16,18 @@ logging.basicConfig(
 )
 
 
+# Main function
+def main() -> None:
+    bot_token = "7866673972:AAFSczpid7J-1vAANUfFgKkq0pxaz-Rc9oA"
+    application = ApplicationBuilder().token(bot_token).build()
+
+    # Handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("bm", bm))
+    application.add_handler(CallbackQueryHandler(handle_callback_query))
+    application.add_handler(MessageHandler(filters.TEXT | filters.PHOTO | filters.VIDEO, handle_message))
+
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
