f8f6a21cba4de43df4ecd9506aeb78d443f81e96
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-12-12 11:48:34 +0530

    Update main.py

--- ShinobiCompass/main.py
+++ ShinobiCompass/main.py
@@ -1,4 +1,7 @@
 import logging
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 from modules.task import settask, schedule, resettask, finv, linv, connect, status
 from modules.start import start, handle_callback_query
 from modules.bm import bm, handle_message
@@ -11,9 +14,9 @@ from telegram.ext import (
     CallbackContext,
     filters,
 )
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 # Configure logging
 logging.basicConfig(
@@ -22,23 +25,24 @@ logging.basicConfig(
 )
 
 # MongoDB setup
-client = MongoClient("mongodb+srv://Akshayofficial:islandbot1904@tgbotproject.uwaxq.mongodb.net/Tgbotproject?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
-db = client.Tgbotproject
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client.get_database("Tgbotproject")
 
 # Main function
 def main() -> None:
-    bot_token = "7866673972:AAFSczpid7J-1vAANUfFgKkq0pxaz-Rc9oA"
+    bot_token = os.getenv("BOT_TOKEN")
     application = ApplicationBuilder().token(bot_token).build()
 
     # Handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("bm", bm))
-    application.add_handler(CommandHanlder("settask", settask))
-    application.add_handler(CommandHanlder("resettask", resettask))
-    application.add_handler(CommandHanlder("finv", fnv))
+    application.add_handler(CommandHandler("settask", settask))
+    application.add_handler(CommandHandler("resettask", resettask))
+    application.add_handler(CommandHandler("finv", finv))
     application.add_handler(CommandHandler("linv", linv))
-    application.add_handler(CommandHanlder("connect", connect))
-    application.add_Handler(CommandHandler("status", status))
+    application.add_handler(CommandHandler("connect", connect))
+    application.add_handler(CommandHandler("status", status))
     application.add_handler(CallbackQueryHandler(handle_callback_query))
     application.add_handler(MessageHandler(filters.TEXT | filters.PHOTO | filters.VIDEO, handle_message))
 
