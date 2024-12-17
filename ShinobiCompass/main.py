3437db8bda6fe8abbbbdfe651b646a9638a064c0
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-12-17 22:58:03 +0530

    Update main.py

--- ShinobiCompass/main.py
+++ ShinobiCompass/main.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
-from ShinobiCompass.modules.task import settask, schedule, resettask, finv, linv, connect, status
 from ShinobiCompass.modules.start import start, handle_callback_query
 from ShinobiCompass.modules.bm import bm, handle_message
 from telegram import Update
@@ -41,16 +40,16 @@ application = ApplicationBuilder().token(BOT_TOKEN).build()
 # Add handlers
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("bm", bm))
-application.add_handler(CommandHandler("settask", settask))
-application.add_handler(CommandHandler("resettask", resettask))
-application.add_handler(CommandHandler("finv", finv))
-application.add_handler(CommandHandler("linv", linv))
-application.add_handler(CommandHandler("connect", connect))
-application.add_handler(CommandHandler("status", status))
-application.add_handler(CommandHandler("schedule", schedule))
-application.add_handler(CallbackQueryHandler(task_replacement_callback, pattern="replace_task|keep_task"))
-application.add_handler(CallbackQueryHandler(button_handler, pattern="submit_task"))
-application.add_handler(CallbackQueryHandler(handle_start, pattern="^connect:"))
+# application.add_handler(CommandHandler("settask", settask))
+# application.add_handler(CommandHandler("resettask", resettask))
+# application.add_handler(CommandHandler("finv", finv))
+# application.add_handler(CommandHandler("linv", linv))
+# application.add_handler(CommandHandler("connect", connect))
+# application.add_handler(CommandHandler("status", status))
+# application.add_handler(CommandHandler("schedule", schedule))
+# application.add_handler(CallbackQueryHandler(task_replacement_callback, pattern="replace_task|keep_task"))
+# application.add_handler(CallbackQueryHandler(button_handler, pattern="submit_task"))
+# application.add_handler(CallbackQueryHandler(handle_start, pattern="^connect:"))
 application.add_handler(CallbackQueryHandler(handle_callback_query))
 application.add_handler(MessageHandler(filters.TEXT | filters.PHOTO | filters.VIDEO, handle_message))
 
